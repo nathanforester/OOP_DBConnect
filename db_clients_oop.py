d0a0c5ec0a5f7b34d11a2c46844209d0191b7215
nathanforester <nfeugene86@gmail.com>
2020-06-10 22:18:00 +0100

    add, read, update functions working, minor error with specific client read function. Runfile test working

--- db_clients_oop.py
+++ db_clients_oop.py
@@ -6,21 +6,15 @@ class DBClientTable(MSDBConnection):
     # def __init__(self):
     #     super().__init__()
 
-    # def create_client(self, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode,
-    #                   Country, Phone, Fax):
-    #     return self.sql_query(f"""INSERT INTO Customers (CompanyName, ContactName, ContactTitle, Address,
-    #                             City, Region, PostalCode, Country, Phone, Fax)
-    #                             VALUES ('{CompanyName}', '{ContactName}', '{ContactTitle}', '{Address}'
-    #                             '{City}', '{Region}', {PostalCode}, '{Country}', '{Phone}', '{Fax}')""").commit
+    def create_client(self, customer_id, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode,
+                      Country, Phone, Fax):
+        return self.sql_query(f"""INSERT INTO Customers (CustomerID, CompanyName, ContactName, ContactTitle, Address,
+                                City, Region, PostalCode, Country, Phone, Fax) VALUES ('{customer_id}',  
+                                '{CompanyName}', '{ContactName}', '{ContactTitle}', '{Address}', '{City}', '{Region}', 
+                                '{PostalCode}', '{Country}', '{Phone}', '{Fax}')""").commit
 
-    def create_entry(self, CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode,
-                     Country, Phone, Fax):
-        return self.sql_query(
-            f"""INSERT INTO Customers (CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax)
-    VALUES (f{CustomerID}, '{CompanyName}', {ContactName}, {ContactTitle},'{Address}', {City}, {Region}, {PostalCode}, {Country}, {Phone}, {Fax})""")
-
-    def get_by_id(self, id):
-        self.sql_query('SELECT * FROM Product WHERE CustomerID =' + str(id)).fetchone()
+    def get_by_id(self, City):
+        self.sql_query(f"SELECT * FROM Customers WHERE City LIKE '%{City}'").fetchall()
 
     def get_all(self, client_name=None):
         result_list = []
@@ -37,4 +31,6 @@ class DBClientTable(MSDBConnection):
 
 
 new_client = DBClientTable()
-print(new_client.create_entry('test', 'test', 'test', 'test', 'test', 'test', '7', 'test', '1', 'test', 'test'))
+print(new_client.get_all())
+# print(new_client.get_by_id("B"))
+# print(new_client.create_client('ab', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'))
